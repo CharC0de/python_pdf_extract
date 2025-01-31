c9a510f454452d6d4ff5bc27183ef0c71a60b865
CharC0de <eganocharlesreinerg@gmail.com>
2025-01-31 21:13:05 +0800

    extracted text

--- app.py
+++ app.py
@@ -331,6 +331,7 @@ def extract_and_transform_table(pdf_path):
         units = extract_key_values(extracted_text)
 
     data = {
+        "text":extracted_text,
         "total_subject_credit": total_subject_credit,
         "total_faculty_credit": total_faculty_credit,
         "total_weekly_hours": row[7].strip(),
